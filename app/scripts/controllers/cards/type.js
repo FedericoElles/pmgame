ed30f50e45a3662338c350bb93bba9d306f13f1c
FedericoElles <github@furikuri.de>
2014-07-14 23:44:35 +0200

    prepared cards/type view for service

--- app/scripts/controllers/cards/type.js
+++ app/scripts/controllers/cards/type.js
@@ -1,8 +1,20 @@
 'use strict';
 
 angular.module('pmgameApp')
-  .controller('CardsTypeCtrl', function ($scope, $routeParams) {
+  .controller('CardsTypeCtrl', function ($scope, $routeParams, $http) {
     $scope.ctrl = {
-      type: $routeParams.type
+      type: $routeParams.type,
+      cards:[]
     };
+    
+    $http.get('http://localhost:8080/data/'+$routeParams.type).
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('Error getting data/type directory', data)
+    });    
   });
