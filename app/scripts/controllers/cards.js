8631e16f0921c6a44d758f6b6c51ec2a3b02a470
FedericoElles <github@furikuri.de>
2014-07-14 23:24:28 +0200

    cards page prepared for API

--- app/scripts/controllers/cards.js
+++ app/scripts/controllers/cards.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('pmgameApp')
-  .controller('CardsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('CardsCtrl', function ($scope, $http) {
+    $scope.types = [
+      'heroes',
+      'quests',
+      'events'
     ];
+    
+    $http.get('http://localhost:8080/data').
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('Error getting data directory', data)
+    });
+    
   });
