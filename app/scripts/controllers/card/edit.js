5bb6f446e0466a5dabf9efe5b617d084d52f8c26
FedericoElles <github@furikuri.de>
2014-08-23 21:25:25 +0200

    editor can now save data

--- app/scripts/controllers/card/edit.js
+++ app/scripts/controllers/card/edit.js
@@ -6,12 +6,32 @@ angular.module('pmgameApp')
       type: $routeParams.type,
       cardId: $routeParams.id,
       card:undefined,
+      cardLive:undefined,
       formHelper:{},
-      saveForm:function(formFields){
-        console.log('Save form callback', formFields);
+      saveForm:function(id, doc){
+        console.log('Save form callback', id, doc);
+        if (id.indexOf('.json') > 0){
+          saveFile(id.replace('.json',''), doc);
+        } else {
+          saveFile('data/'+$routeParams.type+'/'+id, doc);
+        }
       }
     };
-    
+
+    function saveFile(path, data){
+      $http.post('http://localhost:8080/' + path, JSON.stringify(data)).
+      success(function(data, status, headers, config) {
+        console.log('saveFile', data);
+        // this callback will be called asynchronously
+        // when the response is available
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log('Error getting data/type directory', data)
+      });    
+    }
+
     $http.get('http://localhost:8080/data/'+$routeParams.type+'/'+$routeParams.id).
     success(function(data, status, headers, config) {
       $scope.ctrl.card = data;
